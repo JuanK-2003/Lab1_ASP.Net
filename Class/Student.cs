cf541a5a7c72431e9100cfcf5d0cca63626fdb12
Juan Carlos Neíl Palacios Escobar <67353415+JuanK-2003@users.noreply.github.com>
2021-04-04 23:55:43 -0600

    Add files via upload

---
+++ Class/Student.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Lab1_WebForm.Class
+{
+    public class Student
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public string Address { get; set; }
+        public Student()
+        {
+
+        }
+
+        public Student(string Id, string Name = "", string Address = "")
+        {
+            this.Id = Id;
+            this.Name = Name;
+            this.Address = Address;
+        }
+    }
+}
